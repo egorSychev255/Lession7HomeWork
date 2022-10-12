60351797c763b9d229995e27459c56f3c71adb3c
Egor <egorca255@mail.ru>
2022-10-12 15:47:34 +0300

    push

---
+++ prog50/Program.cs
@@ -0,0 +1,43 @@
+﻿
+int[,] MyArray = Myarray( 3 ,3);
+Print2DArray(MyArray);
+FindElement(MyArray);
+int[,]  Myarray(int height, int wight)
+{
+    int[,] TwoDArray = new int [height,wight];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < wight; j++)
+        {
+            TwoDArray[i,j] = new Random().Next(0,10);   
+        }
+    }
+    return TwoDArray;
+}
+
+void FindElement(int[,] array)
+{       
+  for (int i=0;i < array.GetLength(0);i++) 
+    {   
+        int srarfm = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+        
+        srarfm += array[j,i];
+        }  
+         Console.WriteLine($"Среднеарефметическое элементов столбца {i+1} = {(float)srarfm/3}");
+    }
+
+}
+void Print2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] +"\t");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ prog50_2/Program.cs
@@ -0,0 +1,63 @@
+﻿
+Console.WriteLine("введите количество строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите количество столбцов");
+int columns = Convert.ToInt32(Console.ReadLine());
+int[,] UserArray = Myarray(rows, columns);
+Indixes(UserArray);
+Print2DArray(UserArray);
+
+int[,] Myarray(int height, int wight)
+{
+    int[,] TwoDArray = new int[height, wight];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < wight; j++)
+        {
+            TwoDArray[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return TwoDArray;
+}
+void Print2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] Indixes(int[,] array)
+{
+    Console.WriteLine("Введите позицию в массиве");
+    int UserNumber = Convert.ToInt32(Console.ReadLine());
+    if (UserNumber > array.Length)
+    {
+
+        Console.WriteLine("Такого элемента нет");
+    }
+    else
+    {
+         int count = 1;
+        for (int i = 0; i < array.GetLength(1); i++)
+        {
+            for (int j = 0; j < array.GetLength(0); j++)
+            {
+               
+                if (count == UserNumber)
+                {
+                    Console.WriteLine($"Элемент находящийся на позиции {UserNumber} = {array[i, j]}");
+                    
+                }
+                count++;
+            }
+        }
+    }
+
+    return array;
+}
